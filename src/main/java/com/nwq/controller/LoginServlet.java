cdbf9bc7ea722d9ec33bc8033e4ef7387b08eb65
Ning <2232756807@qq.com>
2020-06-23 20:29:38 +0800

    first

---
+++ src/main/java/com/nwq/controller/LoginServlet.java
@@ -0,0 +1,17 @@
+package com.nwq.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getRequestDispatcher("/jsp/common/main.jsp").forward(request, response);
+
+    }
+}
