0d727c7d81a3930d02e4248a9baeef3ee602c9d8
Ning <2232756807@qq.com>
2020-06-23 20:30:55 +0800

    first

---
+++ src/main/java/com/nwq/controller/DeptServlet.java
@@ -0,0 +1,15 @@
+package com.nwq.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/dept/*")
+public class DeptServlet extends BaseServlet{
+    public void list(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("dept list");
+
+    }
+}
