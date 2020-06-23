cab29f66cbaf29b0444946e7a6b892ba6ccb96f0
Ning <2232756807@qq.com>
2020-06-23 20:29:48 +0800

    first

---
+++ src/main/java/com/nwq/controller/User2Servlet.java
@@ -0,0 +1,37 @@
+package com.nwq.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class User2Servlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //  /user/list   /user/add   /user/delete
+        String uri = request.getRequestURI();
+        if (uri.endsWith("list")) {
+            list(request, response);
+        } else if (uri.endsWith("add")) {
+            add(request, response);
+        } else if (uri.endsWith("delete")) {
+            add(request, response);
+        }
+
+    }
+
+    public void list(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getRequestDispatcher("/jsp/user/list.jsp").forward(request, response);
+    }
+
+    public void add(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    public void delete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
